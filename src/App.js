034a6a9868e908a1b9d4dc668609833557088d5e
mossberg500 <axaptasub@gmail.com>
2021-09-07 11:21:27 +0300

    деоение на компоненты 07092021

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
+
+const Technologies =  () => {
+    return(
+        <div className="App">
+            <ui>
+                <li>HTML</li>
+                <li>JS</li>
+            </ui>
+        </div>
+    )
+};
+const Header = () => {
+    return (
+        <div>
+            <p><a href='#'>Home</a></p>
+            <p><a href='#'>New Feed</a></p>
+            <p><a href='#'>Message</a></p>
+        </div>
+    )
+};
 
 export default App;
