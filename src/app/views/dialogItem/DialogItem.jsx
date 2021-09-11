6782b7f9c27def2d61779f62fad8a734bac26f24
mossberg500 <axaptasub@gmail.com>
2021-09-11 10:17:12 +0300

    массив комронентов dialogElements 11092021

--- src/app/views/dialogItem/DialogItem.jsx
+++ src/app/views/dialogItem/DialogItem.jsx
@@ -44,27 +44,36 @@ const DialogItem = (props) => {
         {id: 6, message: 'List'}
     ]
 
+    /* массив реакт jsx компонентов */
+    let dialogElements = [
+        <DialogContext name={dialogContextData[0].name} id={dialogContextData[0].id} />,
+        <DialogContext name={dialogContextData[1].name} id ={dialogContextData[1].id} />,
+        <DialogContext name={dialogContextData[2].name} id ={dialogContextData[2].id} />,
+        <DialogContext name={dialogContextData[3].name} id ={dialogContextData[3].id} />,
+        <DialogContext name={dialogContextData[4].name} id ={dialogContextData[4].id} />,
+        <DialogContext name={dialogContextData[5].name} id ={dialogContextData[5].id} />
+]
 
     return (
         <div className={d.dialogs}>
             <div className={d.dialogsitems}>
-
-                <DialogContext name={dialogContextData[0].name} id={dialogContextData[0].id}/>
+                {dialogElements}
+                {/* <DialogContext name={dialogContextData[0].name} id={dialogContextData[0].id}/>
                 <DialogContext name={dialogContextData[1].name} id={dialogContextData[1].id}/>
                 <DialogContext name={dialogContextData[2].name} id={dialogContextData[2].id}/>
                 <DialogContext name={dialogContextData[3].name} id={dialogContextData[3].id}/>
                 <DialogContext name={dialogContextData[4].name} id={dialogContextData[4].id}/>
-                <DialogContext name={dialogContextData[5].name} id={dialogContextData[5].id}/>
+                <DialogContext name={dialogContextData[5].name} id={dialogContextData[5].id}/>*/}
 
             </div>
 
             <div className={d.message}>
-                <Message message={messageData[0].message}/>
+                {/*<Message message={messageData[0].message}/>
                 <Message message={messageData[1].message}/>
                 <Message message={messageData[2].message}/>
                 <Message message={messageData[3].message}/>
                 <Message message={messageData[4].message}/>
-                <Message message={messageData[5].message}/>
+                <Message message={messageData[5].message}/>*/}
             </div>
 
         </div>
