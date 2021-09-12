19418c49c39c8766e4a96a63d7f02b1f82fa747e
mossberg500 <axaptasub@gmail.com>
2021-09-12 13:49:37 +0300

    state готов 11092021

---
+++ src/app/redux/state.js
@@ -0,0 +1,39 @@
+/*
+let dialogContextData = [
+    {id: 1, name: 'Oleg'},
+    {id: 2, name: 'Andrey'},
+    {id: 3, name: 'Sasha'},
+    {id: 4, name: 'Sveta'},
+    {id: 5, name: 'Victor'},
+    {id: 6, name: 'Vlad'}
+]
+
+let messageData = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How are you'},
+    {id: 3, message: 'Hi hi'},
+    {id: 4, message: 'Good moning'},
+    {id: 5, message: 'Good after'},
+    {id: 6, message: 'List'}
+]
+*/
+
+let state = {
+    dialogContextData: [
+        {id: 1, name: 'Oleg'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sasha'},
+        {id: 4, name: 'Sveta'},
+        {id: 5, name: 'Victor'},
+        {id: 6, name: 'Vlad'}
+        ],
+    messageData: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you'},
+        {id: 3, message: 'Hi hi'},
+        {id: 4, message: 'Good moning'},
+        {id: 5, message: 'Good after'},
+        {id: 6, message: 'List'}
+    ]
+}
+export default state;
--- src/index.js
+++ src/index.js
@@ -3,29 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from "./app/redux/state";
 
+/*
 let dialogContextData = [
-       {id: 1, name: 'Oleg'},
-       {id: 2, name: 'Andrey'},
-       {id: 3, name: 'Sasha'},
-       {id: 4, name: 'Sveta'},
-       {id: 5, name: 'Victor'},
-       {id: 6, name: 'Vlad'}
-   ]
+    {id: 1, name: 'Oleg'},
+    {id: 2, name: 'Andrey'},
+    {id: 3, name: 'Sasha'},
+    {id: 4, name: 'Sveta'},
+    {id: 5, name: 'Victor'},
+    {id: 6, name: 'Vlad'}
+]
 
-   let messageData = [
-       {id: 1, message: 'Hi'},
-       {id: 2, message: 'How are you'},
-       {id: 3, message: 'Hi hi'},
-       {id: 4, message: 'Good moning'},
-       {id: 5, message: 'Good after'},
-       {id: 6, message: 'List'}
-   ]
+let messageData = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How are you'},
+    {id: 3, message: 'Hi hi'},
+    {id: 4, message: 'Good moning'},
+    {id: 5, message: 'Good after'},
+    {id: 6, message: 'List'}
+]
+*/
 
 
 ReactDOM.render(
  <React.StrictMode>
-   <App  dialogContextData={dialogContextData} messageData={messageData} />
+   <App  dialogContextData={state.dialogContextData} messageData={state.messageData} />
  </React.StrictMode>,
  document.getElementById('root')
 );
