828b6b2c8d443691ae90354fd933aa5201ece259
mossberg500 <axaptasub@gmail.com>
2021-10-07 12:40:23 +0300

    Презентационная и Контейнерная компоненты печать 07102021

--- src/app/views/users/UsersContainer.jsx
+++ src/app/views/users/UsersContainer.jsx
@@ -1,10 +1,47 @@
 import React from "react";
-
 import { connect } from "react-redux";
 import {followAC, setPageNumberAC, setTotalElementsAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
-import Users from "./UsersAPIComponent";
-import UsersAPIComponent from "./UsersAPIComponent";
+import * as axios from "axios";
+import Users from "./Users";
 
+class UsersContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        axios.post(`http://localhost:8082/users/get?pageNumber=${this.props.pageNumber}&pageSize=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.content);
+                this.props.setTotalElements(response.data.totalElements);
+
+                console.log(response.data)
+            });
+
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setPageNumber(pageNumber);
+        axios.post(`http://localhost:8082/users/get?pageNumber=${pageNumber}&pageSize=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.content)
+                console.log(response.data)
+            });
+    }
+
+    render() {
+        return <Users totalElements={this.props.totalElements}
+                      pageSize={this.props.pageSize}
+                      pageNumber={this.props.pageNumber}
+                      unfollow={this.props.unfollow}
+                      follow={this.props.follow}
+                      onPageChanged={this.onPageChanged}
+                      users={this.props.users}
+
+        />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -36,4 +73,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
